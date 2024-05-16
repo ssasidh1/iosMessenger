8af4a87d8b8948b0c3a2599ab3c3526ff23bb0e5
Srinidhi Sasidharan <srinidhisasidharan99@gmail.com>
2024-05-16 00:36:57 -0400

    initial commit to remote

---
+++ IOSMessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSMessengerApp
+//
+//  Created by Srinidhi Sasidharan on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
